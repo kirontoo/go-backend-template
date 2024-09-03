85fcc905fe8ca1cda5e822db4fa135608db78a1a
Amy Dang <ndangamy@gmail.com>
2024-09-02 17:10:11 -0700

    fix typo

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -13,7 +13,7 @@ func Equal[T comparable](t testing.TB, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
-		t.Errorf("got: %v; want:%v", actual, expected)
+		t.Errorf("got: %v; want: %v", actual, expected)
 	}
 }
 
